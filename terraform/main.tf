45247368d7f417b65f0859df049d54e6a9342c4b
Fisayo24 <163774684+Fisayo24@users.noreply.github.com>
2026-03-13 07:07:07 -0400

    Create S3 bucket with versioning and encryption
    Added S3 bucket for secure pipeline logs with versioning and encryption.

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,30 @@ secure-cloud-security-pipeline
 ├── diagrams
 │
 └── README.md
+
+resource "aws_s3_bucket" "secure_pipeline_logs" {
+  bucket = "secure-pipeline-logs-demo-12345"
+
+  tags = {
+    Name        = "secure-pipeline-logs"
+    Environment = "security-project"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.secure_pipeline_logs.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.secure_pipeline_logs.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
