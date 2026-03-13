afa6d8ad367e50ec40b7b427b01f62198837a95e
Fisayo24 <163774684+Fisayo24@users.noreply.github.com>
2026-03-13 06:48:34 -0400

    Add main.tf for Terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+secure-cloud-security-pipeline
+│
+├── terraform
+│   ├── main.tf
+│   ├── providers.tf
+│   └── variables.tf
+│
+├── pipeline
+│   └── github-actions.yml
+│
+├── security
+│   ├── tfsec-scan.yml
+│   └── iam-policies
+│
+├── monitoring
+│   └── cloudwatch-alerts.tf
+│
+├── diagrams
+│
+└── README.md
